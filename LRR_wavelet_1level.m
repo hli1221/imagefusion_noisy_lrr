97a1771948001147e218ea4e2515d57b69708eaa
hui.li <hui_li_jnu@163.com>
2018-08-05 15:40:44 +0800

    lrr and wavelet level 1

--- LRR_wavelet_1level.m
+++ LRR_wavelet_1level.m
@@ -1,33 +1,32 @@
-%% LRR_wavelet fusion method
-
-function fusionMap = LRR_wavelet_1level(sourceTestImage1,sourceTestImage2,lam,type)
+%% LRR + wavelet level 1
+function fusionMap = lrr_wavelet_1level(sourceTestImage1,sourceTestImage2,lam, unit,type)
 
 image_data1 = im2double(sourceTestImage1);
 image_data2 = im2double(sourceTestImage2);
 
-% wavelet name
+% wavelet operation
 wname = 'sym2';
-N = 1;
+N = 1;% decomposition level
 [C1,S1] = wavedec2(image_data1,N,wname);
-A1_x1 = appcoef2(C1,S1,wname,N);
-[H1_x1,V1_x1,D1_x1] = detcoef2('all',C1,S1,1);
+A1_x1 = appcoef2(C1,S1,wname,N);% low frequency
+[H1_x1,V1_x1,D1_x1] = detcoef2('all',C1,S1,1);% high frequency for level 1
 
 [C2,S2] = wavedec2(image_data2,N,wname);
-A1_x2 = appcoef2(C2,S2,wname,N);
-[H1_x2,V1_x2,D1_x2] = detcoef2('all',C2,S2,1);
+A1_x2 = appcoef2(C2,S2,wname,N);% low frequency
+[H1_x2,V1_x2,D1_x2] = detcoef2('all',C2,S2,1);% high frequency for level 1
 
 %--------------------------------------------------------------------------------------------%
-%LRR parameter
+% parameter for LRR
 lambda = lam;
-unit = 16;
+% unit = 16;
 
+%% calculate level 1
 [row1,col1] = size(H1_x1);
-
 fusion_A1 = zeros(row1,col1);
 fusion_H1 = zeros(row1,col1);
 fusion_V1 = zeros(row1,col1);
 fusion_D1 = zeros(row1,col1);
-
+% map for low frequency
 mark_A1 = zeros(row1,col1);
 
 % m1 = row1;
@@ -46,9 +45,11 @@ data_H1_x2 = H1_x2;
 data_V1_x2 = V1_x2;
 data_D1_x2 = D1_x2;
 
+% disp('Start');
 tic;
 for i=1:m1
     for j=1:n1
+       % low
         coe_A1_x1 = data_A1_x1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
         coe_A1_x2 = data_A1_x2((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
 
@@ -64,13 +65,11 @@ for i=1:m1
                 mark_A1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit) = zeros(unit);
             end
         end
-
-%         coe_H1_x1 = data_H1_x1((i-4):(i+4),(j-4):(j+4));
-%         coe_H1_x2 = data_H1_x2((i-4):(i+4),(j-4):(j+4));
+        
+        % High - horizontal
         coe_H1_x1 = data_H1_x1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
         coe_H1_x2 = data_H1_x2((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
-
-        
+        % lrr
         [Z1_H1_x1,E1_H1_x1] = solve_lrr(coe_H1_x1,coe_H1_x1,lambda,0,1);
         LRR1_H1_x1 = sum(svd(Z1_H1_x1));
         
@@ -82,12 +81,11 @@ for i=1:m1
         else
             fusion_H1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit) = coe_H1_x2*Z2_H1_x2;
         end
-
-%         coe_V1_x1 = data_V1_x1((i-4):(i+4),(j-4):(j+4));
-%         coe_V1_x2 = data_V1_x2((i-4):(i+4),(j-4):(j+4));
+        
+        % High - vertical
         coe_V1_x1 = data_V1_x1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
         coe_V1_x2 = data_V1_x2((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
-
+        %lrr
         [Z1_V1_x1,E1_V1_x1] = solve_lrr(coe_V1_x1,coe_V1_x1,lambda,0,1);
         [Z2_V1_x2,E2_V1_x2] = solve_lrr(coe_V1_x2,coe_V1_x2,lambda,0,1);
         
@@ -99,12 +97,11 @@ for i=1:m1
         else
             fusion_H1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit) = coe_V1_x2*Z2_V1_x2;
         end
-
-%         coe_D1_x1 = data_D1_x1((i-4):(i+4),(j-4):(j+4));
-%         coe_D1_x2 = data_D1_x2((i-4):(i+4),(j-4):(j+4));
+        
+        % Hogh - diagonal
         coe_D1_x1 = data_D1_x1((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
         coe_D1_x2 = data_D1_x2((i-1)*unit+1:i*unit,(j-1)*unit+1:j*unit);
-
+        %lrr
         [Z1_D1_x1,E1_D1_x1] = solve_lrr(coe_D1_x1,coe_D1_x1,lambda,0,1);
         [Z2_D1_x2,E2_D1_x2] = solve_lrr(coe_D1_x2,coe_D1_x2,lambda,0,1);
         
@@ -118,15 +115,11 @@ for i=1:m1
         end
     end
 end
-toc;
-
+% toc;
+% disp('Done');
 
 fusion_A1 = mark_A1.*A1_x1 + (1-mark_A1).*A1_x2;
-% fusion_H1 = mark_H1.*H1_x1 + (1-mark_H1).*H1_x2;
-% fusion_V1 = mark_V1.*V1_x1 + (1-mark_V1).*V1_x2;
-% fusion_D1 = mark_D1.*D1_x1 + (1-mark_D1).*D1_x2;
-
-
+% Inverse DWT
 [f_m,f_n] = size(image_data1);
 
 S_temp = S1;
@@ -141,8 +134,8 @@ C_temp(3*len1+1:4*len1) = fusion_D1(:);
 
 fusionImage = waverec2(C_temp,S_temp,wname);
 
-figure;
-imshow((fusionImage));
+% figure;
+% imshow((fusionImage));
 
 fusionMap = (fusionImage);
 end
